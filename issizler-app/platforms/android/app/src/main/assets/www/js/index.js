290069ad74e5dc4f81096e9997ef37fdf4383606
katmanco <furkan.katman@gmail.com>
2020-10-28 21:45:48 +0300

    Profil sayfası yapıldı ana ekrana birkaç düzenleme yapıldı. dolar:8.2775TL

--- issizler-app/platforms/android/app/src/main/assets/www/js/index.js
+++ issizler-app/platforms/android/app/src/main/assets/www/js/index.js
@@ -62,6 +62,7 @@ var app = {
             /*
            SideNavEnd 
            */
+            $scope.state = $state;
             $scope.fib = firebase;
             $scope.fib.db = firebase.database();
             $scope.los = localStorageService;
@@ -91,7 +92,114 @@ var app = {
 
 
         })
+        app.controller("ProfileController", function ($scope, $mdDialog) {
+            const profileRef = $scope.fib.db.ref("Users");
+            $scope.isEdit = false;
+            $scope.User = {};
+            profileRef.orderByChild("Uid").equalTo($scope.los.get("User").uid).on("value", function (snapshot) {
+                snapshot.forEach((val) => {
+                    $scope.User = val.val()
+                })
+                setTimeout(() => {
+                    $scope.$apply()
+                }, 100);
+            })
 
+            $scope.UpdateProfile = function () {
+                if ($scope.ProfileImageBlob != null) {
+                    $scope.isUploading = true;
+                    var fileName = $scope.User.Name + $scope.User.Surname
+                        + "_" + moment().valueOf() + "." + $scope.ProfileImageBlob.type.substr($scope.ProfileImageBlob.type.indexOf("/") + 1)
+                    const imageRef = firebase.storage().ref().child("ProfileImages").child($scope.los.get("User").uid);
+                    imageRef.child(fileName).put($scope.ProfileImageBlob).then(function (snapshot) {
+                        snapshot.ref.getDownloadURL().then(function (downloadURLF) {
+                            $scope.User.ProfileImage = downloadURLF;
+                            profileRef.orderByChild("Uid").equalTo($scope.los.get("User").uid).once("value").then(function (snapshot) {
+                                console.log(snapshot.val())
+                                profileRef.child(Object.keys(snapshot.val())[0]).update(angular.copy($scope.User)).then(function () {
+                                    setTimeout(() => {
+                                        $scope.isEdit = false;
+                                        $scope.$apply();
+                                    }, 100);
+                                })
+                            })
+                        })
+
+                    });
+                }
+                else {
+                    profileRef.orderByChild("Uid").equalTo($scope.los.get("User").uid).once("value").then(function (snapshot) {
+                        console.log(snapshot.val())
+                        profileRef.child(Object.keys(snapshot.val())[0]).update(angular.copy($scope.User)).then(function () {
+                            setTimeout(() => {
+                                $scope.isEdit = false;
+                                $scope.$apply();
+                            }, 100);
+                        })
+                    })
+                }
+
+            }
+            $scope.PickProfilePhoto = function () {
+
+                function setOptions(srcType) {
+                    var options = {
+                        // Some common settings are 20, 50, and 100
+                        quality: 50,
+                        destinationType: Camera.DestinationType.FILE_URI,
+                        // In this app, dynamically set the picture source, Camera or photo gallery
+                        sourceType: srcType,
+                        encodingType: Camera.EncodingType.JPEG,
+                        mediaType: Camera.MediaType.PICTURE,
+                        allowEdit: true,
+                        correctOrientation: true  //Corrects Android orientation quirks
+                    }
+                    return options;
+                }
+                var srcType = Camera.PictureSourceType.CAMERA;
+                var options = setOptions(srcType);
+                function cameraSuccess(imageURI) {
+                    $scope.Uploading = true;
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", imageURI);
+                    xhr.responseType = "blob";
+                    xhr.onload = response;
+                    xhr.send();
+                    $scope.$apply();
+                }
+                function cameraError(message) {
+
+                }
+                function response(e) {
+                    var urlCreator = window.URL || window.webkitURL;
+                    $scope.ProfileImagePreview = urlCreator.createObjectURL(this.response);
+                    $scope.ProfileImageBlob = this.response;
+
+                    setTimeout(() => {
+                        $scope.$apply()
+                    }, 100);
+                }
+
+                var confirm = $mdDialog.confirm()
+                    .title('Fotoraf Seç')
+                    .textContent('')
+                    .ariaLabel('Lucky day')
+                    .targetEvent(null)
+                    .ok('Kamera')
+                    .cancel('Galeri');
+                $mdDialog.show(confirm).then(function () {
+                    options = setOptions(navigator.camera.PictureSourceType.CAMERA)
+                    navigator.camera.getPicture(cameraSuccess, cameraError, options);
+                    console.log("removed", op);
+                }, function () {
+                    options = setOptions(navigator.camera.PictureSourceType.PHOTOLIBRARY)
+                    navigator.camera.getPicture(cameraSuccess, cameraError, options);
+                });
+
+            }
+
+
+        })
 
 
 
@@ -173,12 +281,12 @@ var app = {
                     var providerData = user.providerData;
                     console.log("innn", user);
                     $scope.los.set("User", user);
-                    const ProfileRef = $scope.fib.db.ref("Users").child(user.uid);
+                    const ProfileRef = $scope.fib.db.ref("Users");
 
-                    ProfileRef.once("value").then(function (snapshot) {
+                    ProfileRef.orderByChild("Uid").equalTo(uid).once("value").then(function (snapshot) {
                         if (snapshot.hasChildren() == false) {
                             var key = ProfileRef.push().key;
-                            ProfileRef.child(key).set({ Email: user.email });
+                            ProfileRef.child(key).set({ Email: user.email, Uid: uid, Created: moment().valueOf() });
                         }
                     })
                     if ($scope.fib.auth().currentUser.emailVerified == false) {
